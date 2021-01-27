86c17f4a0eed5efa54bf7678d91c1760293d0467
ezzygit <ezzaemails@gmail.com>
2021-01-27 11:10:39 +0000

    works

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,18 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
-const uri = "mongodb+srv://erin:Test123@cluster0.ktcah.mongodb.net/cnmaster?retryWrites=true&w=majority";
-
 const connection = async () => {
     try { 
-    await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect(process.env.URI, {useNewUrlParser: true, useUnifiedTopology: true});
     console.log("connection made");
     } catch (error) {
         console.log(error);
     }
 };
-
 
 connection();
 
